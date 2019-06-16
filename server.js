733c6bd2b738453dde3f5ea049f5210f7ee12e9c
acl <acl447@nyu.edu>
2019-06-16 16:40:52 -0700

    initialized express

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ let axios = require("axios");
 
 let cheerio = require("cheerio");
 
+//Require all models
+let db = require("./models");
 
+let PORT = process.env.PORT || 3000;
 
+//initialize Express
+let app = express();
 
+//parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Make public a static folder
+app.use(express.static("public"));
 
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
