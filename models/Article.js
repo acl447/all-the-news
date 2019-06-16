f4964ff14671336e1b02f2134f52e071fa5976d7
acl <acl447@nyu.edu>
2019-06-16 16:14:50 -0700

    created Article model

---
+++ models/Article.js
@@ -0,0 +1,43 @@
+let mongoose = require("mongoose");
+
+//Save a reference to the Schema constructor
+let Schema = mongoose.Schema;
+
+//Using the Schema constructor, create a new ArticleSchema object
+let ArticleSchema = new Schema({
+
+    headline: {
+
+        type: String,
+        required: true
+
+    },
+
+    summary: {
+
+        type: String,
+        required: true
+    },
+
+    url: {
+
+        type: String,
+        required: true
+    },
+
+    note: {
+
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+
+    }
+
+
+
+});
+
+//creates model from the above schema, using mongoose's model method
+let Article = mongoose.model("Article", ArticleSchema);
+
+//Export the Article model
+module.exports = Article;
