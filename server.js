b7e601ef2df4a1c017b3f9ed3198ca6d37417fe8
acl <acl447@nyu.edu>
2019-06-16 22:16:53 -0700

    function added

--- server.js
+++ server.js
@@ -108,22 +108,22 @@ app.get("/articles/:id", function (req, res) {
 });
 
 //Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
 
     //Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
-        .then(function(dbNote) {
+        .then(function (dbNote) {
 
             //If a Note was created successfully, find one Article with an _id equal to req.params.id.
             //Update the Article to be associated with the new Note 
             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
 
             //If able to successfully update an Article, send it back to the client 
             res.json(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
 
             //If an error occurred, send it to the client
             res.json(err);
@@ -135,7 +135,7 @@ app.post("/articles/:id", function(req, res) {
 });
 
 //Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 
     console.log("App running on PORT " + PORT + "!");
 
