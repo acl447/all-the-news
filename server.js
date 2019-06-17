86e5cf9abbb812a816d6c0802529ef910c235902
acl <acl447@nyu.edu>
2019-06-16 17:53:07 -0700

    created GET route for scraping

--- server.js
+++ server.js
@@ -25,3 +25,50 @@ let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 //connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
+
+//Routes
+
+//A GET route for scraping the digg website
+app.get("/scrape", function (req, res) {
+
+
+    axios.get("http://digg.com/").then(function (response) {
+
+
+        let $ = cheerio.load(response.data);
+
+        //grab every h2 within an article tag, and do the following:
+        $("article h2").each(function (i, element) {
+
+            //Save an empty result object
+            let result = {};
+
+            //Add the text and href of every link, and save them as properties of the result
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.url = $(this)
+                .children("a")
+                .attr("href");
+
+
+            //Create a new Article using the result object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+
+                    console.log(err);
+
+                });
+
+        });
+
+        //Send a message to the client
+        res.send("Scrape complete");
+
+    });
+
+});
