e1c0c12ec68e313ec3d15925db1d30839b80401b
acl <acl447@nyu.edu>
2019-06-16 19:48:49 -0700

    created GET route to grab article by id

--- server.js
+++ server.js
@@ -72,3 +72,71 @@ app.get("/scrape", function (req, res) {
     });
 
 });
+
+//Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+
+
+    //Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+
+            res.json(dbArticle);
+
+
+        })
+        .catch(function (err) {
+
+            res.json(err);
+        });
+});
+
+//Route for grabbing a specific Article by id, populating it with its note 
+app.get("/articles/:id", function (req, res) {
+
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+
+            res.json(dbArticle);
+
+        })
+        .catch(function (err) {
+
+            res.json(err);
+        });
+});
+
+//Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+
+    //Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+
+            //If a Note was created successfully, find one Article with an _id equal to req.params.id.
+            //Update the Article to be associated with the new Note 
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function(dbArticle) {
+
+            //If able to successfully update an Article, send it back to the client 
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+
+            //If an error occurred, send it to the client
+            res.json(err);
+
+        });
+
+
+
+});
+
+//Start the server
+app.listen(PORT, function() {
+
+    console.log("App running on PORT " + PORT + "!");
+
+});
